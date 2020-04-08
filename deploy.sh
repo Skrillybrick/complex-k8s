962c30c80ead04542f3f842aa27be74d05f5b55e
Chase Smith <casmith@solutionreach.com>
2020-04-08 12:43:02 -0600

    hopefully fixing script for real this time

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,16 @@
-docker build -t skrillybrick/complex-client:latest -t skrillybrick/complex-client:$GIT_SHA -f ./client/Dockerfile ./client
-docker build -t skrillybrick/complex-server:latest -t skrillybrick/complex-server:$GIT_SHA -f ./server/Dockerfile ./server
-docker build -t skrillybrick/complex-worker:latest -t skrillybrick/complex-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+docker build -t skrillybrick/complex-client:latest -t skrillybrick/complex-client:$SHA -f ./client/Dockerfile ./client
+docker build -t skrillybrick/complex-server:latest -t skrillybrick/complex-server:$SHA -f ./server/Dockerfile ./server
+docker build -t skrillybrick/complex-worker:latest -t skrillybrick/complex-worker:$SHA -f ./worker/Dockerfile ./worker
 
 docker push skrillybrick/complex-client:latest
 docker push skrillybrick/complex-server:latest
 docker push skrillybrick/complex-worker:latest
-docker push skrillybrick/complex-client:$GIT_SHA
-docker push skrillybrick/complex-server:$GIT_SHA
-docker push skrillybrick/complex-worker:$GIT_SHA
+
+docker push skrillybrick/complex-client:$SHA
+docker push skrillybrick/complex-server:$SHA
+docker push skrillybrick/complex-worker:$SHA
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=skrillybrick/complex-server:$GIT_SHA
-kubectl set image deployments/client-deployment server=skrillybrick/complex-client:$GIT_SHA
-kubectl set image deployments/worker-deployment server=skrillybrick/complex-worker:$GIT_SHA
+kubectl set image deployments/server-deployment server=skrillybrick/complex-server:$SHA
+kubectl set image deployments/client-deployment client=skrillybrick/complex-client:$SHA
+kubectl set image deployments/worker-deployment worker=skrillybrick/complex-worker:$SHA
